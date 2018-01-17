3323faf3f5179a7344ea70d600f17002bf609d29
sue445 <sue445@sue445.net>
2018-01-17 22:03:53 +0900

    Notify message on task is failed

--- lib/capistrano/around_chatwork.rb
+++ lib/capistrano/around_chatwork.rb
@@ -3,8 +3,19 @@ require "cha"
 
 module Capistrano
   module AroundChatwork
-    def self.format_message(message:, task_name:, elapsed_time: 0)
-      message.gsub(":task_name:", task_name).gsub(":elapsed_time:", sprintf("%5.3f", elapsed_time))
+    def self.format_message(message:, task_name:, elapsed_time: 0, error: nil)
+      formatted_message = message.
+        gsub(":task_name:", task_name).
+        gsub(":elapsed_time:", sprintf("%5.3f", elapsed_time))
+
+      if error
+        formatted_message = formatted_message.
+          gsub(":error_message:", error.message).
+          gsub(":error_inspect:", error.inspect).
+          gsub(":error_backtrace:", error.backtrace.join("\n"))
+      end
+
+      formatted_message
     end
 
     def self.post_chatwork(message)
@@ -36,7 +47,29 @@ def around_chatwork(task_name)
     Capistrano::AroundChatwork.post_chatwork(message)
   end
 
-  Rake::Task[task_name].enhance([before_task]) do
+  target_task = Rake::Task[task_name]
+
+  target_task.instance_eval do
+    define_singleton_method :invoke_with_failure_message do |*args|
+      begin
+        invoke_without_failure_message(*args)
+      rescue Exception => error
+        elapsed_time = Time.now - start_time
+        message = Capistrano::AroundChatwork.format_message(
+          message:      fetch(:failure_message),
+          task_name:    task_name,
+          elapsed_time: elapsed_time,
+          error:        error,
+        )
+        Capistrano::AroundChatwork.post_chatwork(message)
+        raise
+      end
+    end
+    alias :invoke_without_failure_message :invoke
+    alias :invoke :invoke_with_failure_message
+  end
+
+  target_task.enhance([before_task]) do
     Rake::Task[after_task].invoke
   end
 end
@@ -47,6 +80,14 @@ set :starting_message, -> {
 
 set :ending_message, -> {
   "[info][title][#{fetch(:stage)}] :task_name: @#{fetch(:user)}[/title]done (:elapsed_time: sec)[/info]"
+}
+
+set :failure_message, -> {
+  <<-MSG
+[info][title][#{fetch(:stage)}] :task_name: @#{fetch(:user)}[/title]failed (:elapsed_time: sec)
+:error_inspect:
+:error_backtrace:[/info]
+  MSG
 }
 
 set :user, local_user
